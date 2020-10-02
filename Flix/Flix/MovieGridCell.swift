0e3453444b5d2d05380067ab9c6a7f8325551437
Lakshmi Kala Pedarla <kala.pedarla18@gmail.com>
2020-10-01 20:49:41 -0700

    Flix2

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Lakshmi Kala on 10/1/20.
+//  Copyright © 2020 kala.pedarla18@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
